68c66aa0de981242d09fd0222e4121c547e4bd10
Mohan Sagar <mohansgr3@gmail.com>
2024-03-28 11:39:59 +0530

    footer padding updated

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-background h-[100px] text-typography">
+    <footer className="bg-background h-[100px] p-5 pr-20 text-typography">
       <p className="text-primary">Author: Mohansagar K.</p>
       <p className="text-primary">
         <a href="mailto:contact@mohansagar.website">
