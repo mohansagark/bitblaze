dcae4d30201eab107e30ea28bc3bd891577267b4
Mohan Sagar <mohansgr3@gmail.com>
2025-06-29 09:28:36 +0530

    set dark mode as default mode

--- src/themes/defaultTheme.js
+++ src/themes/defaultTheme.js
@@ -1,4 +1,4 @@
 import { COLORS, THEMES } from "../helpers/config";
 
 export const DEFAULT_COLOR = COLORS.green;
-export const DEFAULT_MODE = THEMES.LIGHT;
+export const DEFAULT_MODE = THEMES.DARK;
